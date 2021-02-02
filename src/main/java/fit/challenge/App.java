f5a8163c9bdc67e081b4c066a37fdeae289c62d3
Nikolas Sugo <nikolas.sugo@gmail.com>
2021-02-01 22:56:24 -0300

    Initial Commit

--- src/main/java/fit/challenge/App.java
+++ src/main/java/fit/challenge/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
+        // String result = ImcCalculator.calculate(88, 1.77);
+
+        // System.out.println( result );
     }
 }
